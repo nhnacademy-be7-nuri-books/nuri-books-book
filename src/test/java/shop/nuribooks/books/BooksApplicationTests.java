4f53f8b8ce760acff758eb2633d581b94e8137c8
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-10-21 10:39:31 +0900

    [INIT] start project!!

---
+++ src/test/java/shop/nuribooks/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.nuribooks.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
