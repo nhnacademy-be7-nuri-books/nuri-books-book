53318d037868eadb973b65343511e491c7ebb2ae
jaaaayk <janghyun30@gmail.com>
2024-10-23 16:48:36 +0900

    refactor: GlobalExceptionHandler errordto 변경

--- src/main/java/shop/nuribooks/books/controller/GlobalExceptionHandler.java
+++ src/main/java/shop/nuribooks/books/controller/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-import shop.nuribooks.books.dto.error.ErrorRes;
+import shop.nuribooks.books.dto.error.ErrorResponse;
 import shop.nuribooks.books.exception.BadRequestException;
 import shop.nuribooks.books.exception.ResourceAlreadyExistException;
 import shop.nuribooks.books.exception.ResourceNotFoundException;
@@ -16,34 +16,35 @@ import shop.nuribooks.books.exception.ResourceNotFoundException;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<ErrorRes> handleResourceNotFoundException(ResourceNotFoundException ex,
+	public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex,
 		WebRequest request) {
 		return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
 	}
 
 	@ExceptionHandler(BadRequestException.class)
-	public ResponseEntity<ErrorRes> handleInvalidDataException(BadRequestException ex, WebRequest request) {
+	public ResponseEntity<ErrorResponse> handleInvalidDataException(BadRequestException ex, WebRequest request) {
 		return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorRes> handleValidationException(MethodArgumentNotValidException ex,
+	public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex,
 		WebRequest request) {
 		return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
 	}
 
 	@ExceptionHandler(ResourceAlreadyExistException.class)
-	public ResponseEntity<ErrorRes> handleDuplicateException(ResourceAlreadyExistException ex, WebRequest request) {
+	public ResponseEntity<ErrorResponse> handleDuplicateException(ResourceAlreadyExistException ex,
+		WebRequest request) {
 		return buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage(), request);
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorRes> handleGlobalException(Exception ex, WebRequest request) {
+	public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
 		return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred", request);
 	}
 
-	private ResponseEntity<ErrorRes> buildErrorResponse(HttpStatus status, String message, WebRequest request) {
-		ErrorRes errorResponse = new ErrorRes(
+	private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus status, String message, WebRequest request) {
+		ErrorResponse errorResponse = new ErrorResponse(
 			status.value(),
 			message,
 			request.getDescription(false)
