7cda1f6701dab69766f54ca6a884734b2dea873e
jaaaayk <janghyun30@gmail.com>
2024-10-24 15:25:36 +0900

    feat: CategoryController에 수정 추가

--- src/main/java/shop/nuribooks/books/controller/category/CategoryController.java
+++ src/main/java/shop/nuribooks/books/controller/category/CategoryController.java
@@ -7,11 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
@@ -113,6 +115,31 @@ public class CategoryController {
 	public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable Long categoryId) {
 		CategoryResponse categoryResponse = categoryService.getCategoryById(categoryId);
 		return ResponseEntity.ok(categoryResponse);
+	}
+
+	/**
+	 * 특정 카테고리를 업데이트합니다.
+	 *
+	 * @author janghyun
+	 * @param dto 업데이트할 카테고리의 정보
+	 * @param categoryId 업데이트할 카테고리의 ID
+	 * @return 업데이트된 카테고리의 응답 DTO
+	 */
+	@Operation(summary = "카테고리 업데이트", description = "주어진 ID에 해당하는 카테고리를 업데이트합니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "성공적으로 카테고리를 업데이트하였습니다."),
+		@ApiResponse(responseCode = "404", description = "해당 ID의 카테고리를 찾을 수 없습니다."),
+		@ApiResponse(responseCode = "400", description = "잘못된 요청입니다.")
+	})
+	@PutMapping("/{categoryId}")
+	public ResponseEntity<CategoryResponse> updateCategory(
+		@Valid
+		@Parameter(description = "업데이트할 카테고리의 정보", required = true)
+		@RequestBody CategoryRequest dto,
+		@Parameter(description = "업데이트할 카테고리의 ID", required = true)
+		@PathVariable Long categoryId) {
+
+		return ResponseEntity.ok(categoryService.updateCategory(dto, categoryId));
 	}
 
 }
