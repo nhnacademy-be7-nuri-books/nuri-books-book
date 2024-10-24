b9222f2f97418d023f2e30704ca584550748243e
kkyongg <kkyongg2@gmail.com>
2024-10-24 17:11:43 +0900

    feat: publisher 조회 기능 테스트코드 작성

--- src/main/java/shop/nuribooks/books/service/publisher/PublisherService.java
+++ src/main/java/shop/nuribooks/books/service/publisher/PublisherService.java
@@ -6,7 +6,7 @@ import shop.nuribooks.books.dto.publisher.PublisherRequest;
 import shop.nuribooks.books.dto.publisher.PublisherResponse;
 
 public interface PublisherService {
-	//TODO: 등록, 조회, 수정, 삭제 구현
+	//TODO: 수정, 삭제 구현
 	PublisherResponse registerPublisher(PublisherRequest request);
 
 	List<PublisherResponse> getAllPublisher();
--- src/test/java/shop/nuribooks/books/service/publisher/PublisherServiceImplTest.java
+++ src/test/java/shop/nuribooks/books/service/publisher/PublisherServiceImplTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -14,6 +17,7 @@ import shop.nuribooks.books.dto.publisher.PublisherRequest;
 import shop.nuribooks.books.dto.publisher.PublisherResponse;
 import shop.nuribooks.books.entity.book.Publisher;
 import shop.nuribooks.books.exception.publisher.PublisherAlreadyExistsException;
+import shop.nuribooks.books.exception.publisher.PublisherNotFoundException;
 import shop.nuribooks.books.repository.publisher.PublisherRepository;
 
 @SpringBootTest
@@ -56,24 +60,76 @@ class PublisherServiceImplTest {
 		when(publisherRepository.existsByName(request.name())).thenReturn(true);
 
 		//then
-		assertThatThrownBy(() -> publisherService.registerPublisher(request))
-			.isInstanceOf(PublisherAlreadyExistsException.class)
-			.hasMessageContaining("출판사가 이미 등록되어 있습니다.");
+		assertThatThrownBy(() -> publisherService.registerPublisher(request)).isInstanceOf(
+			PublisherAlreadyExistsException.class).hasMessageContaining("출판사가 이미 등록되어 있습니다.");
 
 		// 메서드 호출 여부 검증
 		verify(publisherRepository).existsByName(request.name());
 		verify(publisherRepository, never()).save(any(Publisher.class));
 	}
 
+	@DisplayName("모든 출판사 조회 성공")
+	@Test
+	void getAllPublisher() {
+		// given
+		List<Publisher> publishers = List.of(new Publisher(1L, "publisher1"), new Publisher(2L, "publisher2"));
+
+		when(publisherRepository.findAll()).thenReturn(publishers);
+
+		// when
+		List<PublisherResponse> responses = publisherService.getAllPublisher();
+
+		// then
+		assertThat(responses).isNotEmpty();
+		assertThat(responses).hasSize(2);
+		assertThat(responses.get(0).name()).isEqualTo("publisher1");
+
+		// 메서드 호출 여부 검증
+		verify(publisherRepository).findAll();
+	}
+
+	@DisplayName("검색한 출판사 조회 성공")
+	@Test
+	void getPublisher() {
+		// given
+		String publisherName = "publisher1";
+		Publisher publisher = new Publisher(1L, publisherName);
+
+		when(publisherRepository.findByName(publisherName)).thenReturn(Optional.of(publisher));
+
+		// when
+		PublisherResponse response = publisherService.getPublisher(publisherName);
+
+		// then
+		assertThat(response).isNotNull();
+		assertThat(response.name()).isEqualTo(publisherName);
+
+		// 메서드 호출 여부 검증
+		verify(publisherRepository).findByName(publisherName);
+	}
+
+	@DisplayName("검색한 출판사 조회 실패 - 존재하지 않음")
+	@Test
+	void failed_getPublisher() {
+		// given
+		String publisherName = "unknownPublisher";
+
+		when(publisherRepository.findByName(publisherName)).thenReturn(Optional.empty());
+
+		// then
+		assertThatThrownBy(() -> publisherService.getPublisher(publisherName)).isInstanceOf(
+			PublisherNotFoundException.class).hasMessageContaining("출판사가 존재하지 않습니다.");
+
+		verify(publisherRepository).findByName(publisherName);
+
+	}
+
 	private PublisherRequest registerRequest() {
-		return PublisherRequest.builder()
-			.name("publisher1")
-			.build();
+		return PublisherRequest.builder().name("publisher1").build();
 	}
 
 	private Publisher savedPublisher() {
-		return Publisher.builder()
-			.name("publisher1")
+		return Publisher.builder().name("publisher1")
 			.build();
 	}
 }
