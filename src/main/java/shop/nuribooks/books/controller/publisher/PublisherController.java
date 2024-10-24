460e483b43b424c8253ec90eb2bdf431d609b415
kkyongg <kkyongg2@gmail.com>
2024-10-24 17:45:14 +0900

    feat: swagger 추가

--- src/main/java/shop/nuribooks/books/controller/publisher/PublisherController.java
+++ src/main/java/shop/nuribooks/books/controller/publisher/PublisherController.java
@@ -89,6 +89,12 @@ public class PublisherController {
 	 * @param publisherName 삭제할 출판사 이름
 	 * @return HttpStatus
 	 */
+	@Operation(summary = "출판사 삭제", description = "주어진 이름의 출판사를 삭제합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "출판사 삭제 성공"),
+		@ApiResponse(responseCode = "404", description = "출판사 미존재"),
+		@ApiResponse(responseCode = "500", description = "서버 오류")
+	})
 	@DeleteMapping("/api/publishers/{publisherName}")
 	public ResponseEntity<HttpStatus> deletePublisher(@PathVariable String publisherName) {
 		publisherService.deletePublisher(publisherName);
