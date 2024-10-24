f0a9f04fab0f6da0df0b7b388de57cb8186f1320
Jprotection <bigho98@gmail.com>
2024-10-24 13:40:35 +0900

    test: memberService의 모든 기능의 실패 상황 테스트 코드 작성

--- src/test/java/shop/nuribooks/books/service/member/MemberServiceImplTest.java
+++ src/test/java/shop/nuribooks/books/service/member/MemberServiceImplTest.java
@@ -2,6 +2,7 @@ package shop.nuribooks.books.service.member;
 
 import static java.math.BigDecimal.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static shop.nuribooks.books.entity.member.AuthorityEnum.*;
 import static shop.nuribooks.books.entity.member.GradeEnum.*;
@@ -26,6 +27,12 @@ import shop.nuribooks.books.dto.member.response.MemberRegisterResponse;
 import shop.nuribooks.books.dto.member.response.MemberUpdateResponse;
 import shop.nuribooks.books.entity.member.Customer;
 import shop.nuribooks.books.entity.member.Member;
+import shop.nuribooks.books.exception.member.CustomerNotFoundException;
+import shop.nuribooks.books.exception.member.EmailAlreadyExistsException;
+import shop.nuribooks.books.exception.member.InvalidPasswordException;
+import shop.nuribooks.books.exception.member.MemberNotFoundException;
+import shop.nuribooks.books.exception.member.UserIdAlreadyExistsException;
+import shop.nuribooks.books.exception.member.UserIdNotFoundException;
 import shop.nuribooks.books.repository.member.CustomerRepository;
 import shop.nuribooks.books.repository.member.MemberRepository;
 
@@ -74,6 +81,35 @@ class MemberServiceImplTest {
 		verify(memberRepository, times(1)).save(any(Member.class));
 	}
 
+	@DisplayName("회원 등록 실패 - 중복된 이메일")
+	@Test
+	public void registerMember_EmailAlreadyExists() {
+	    //given
+		MemberRegisterRequest request = memberCreateRequest();
+		when(customerRepository.existsByEmail(request.getEmail())).thenReturn(true);
+
+	    //when / then
+		EmailAlreadyExistsException exception = assertThrows(EmailAlreadyExistsException.class,
+			() -> memberServiceImpl.registerMember(request));
+		assertThat(exception.getMessage()).isEqualTo("이미 존재하는 이메일입니다.");
+
+	}
+
+	@DisplayName("회원 등록 실패 - 중복된 아이디")
+	@Test
+	void registerMember_UserIdAlreadyExists() {
+	    //given
+		MemberRegisterRequest request = memberCreateRequest();
+
+		when(customerRepository.existsByEmail(request.getEmail())).thenReturn(false);
+		when(memberRepository.existsByUserId(request.getUserId())).thenReturn(true);
+
+		//when / then
+		UserIdAlreadyExistsException exception = assertThrows(UserIdAlreadyExistsException.class,
+			() -> memberServiceImpl.registerMember(request));
+		assertThat(exception.getMessage()).isEqualTo("이미 존재하는 아이디입니다.");
+	}
+
 	@DisplayName("회원 탈퇴 성공")
 	@Test
 	void withdrawMember() {
@@ -96,9 +132,43 @@ class MemberServiceImplTest {
 		assertThat(existingMember.getWithdrawnAt()).isNotNull(); // withdrawnAt이 현재 시간으로 설정되었는지 확인
 	}
 
+	@DisplayName("회원 탈퇴 실패 - 존재하지 않는 아이디")
+	@Test
+	void withdrawMember_UserIdNotFound() {
+	    //given
+		MemberWithdrawRequest request = memberWithdrawRequest();
+
+		when(memberRepository.existsByUserId(request.getUserId())).thenReturn(false);
+
+	    //when / then
+		UserIdNotFoundException exception = assertThrows(UserIdNotFoundException.class,
+			() -> memberServiceImpl.withdrawMember(request));
+		assertThat(exception.getMessage()).isEqualTo("존재하지 않는 아이디입니다.");
+	}
+
+	@DisplayName("회원 탈퇴 실패 - 비밀번호 불일치")
+	@Test
+	void withdrawMember_InvalidPassword() {
+		//given
+		MemberWithdrawRequest request = memberWithdrawRequest();
+
+		Customer existingCustomer = customer();
+		Member existingMember = member(existingCustomer);
+
+		when(memberRepository.existsByUserId(request.getUserId())).thenReturn(true);
+		when(memberRepository.findByUserId(request.getUserId())).thenReturn(Optional.of(existingMember));
+		when(customerRepository.existsByIdAndPassword(existingMember.getId(), request.getPassword()))
+			.thenReturn(false);
+
+		//when / then
+		InvalidPasswordException exception = assertThrows(InvalidPasswordException.class,
+			() -> memberServiceImpl.withdrawMember(request));
+		assertThat(exception.getMessage()).isEqualTo("비밀번호가 일치하지 않습니다.");
+	}
+
 	@DisplayName("회원 정보 수정 성공")
 	@Test
-	public void updateMember() {
+	void updateMember() {
 	    //given
 		MemberUpdateRequest request = memberUpdateRequest();
 		Customer existingCustomer = spy(customer());
@@ -117,9 +187,43 @@ class MemberServiceImplTest {
 		assertThat(response.getPhoneNumber()).isEqualTo(existingCustomer.getPhoneNumber());
 	}
 
+	@DisplayName("회원 정보 수정 실패 - 존재하지 않는 회원")
+	@Test
+	void updateMember_MemberNotFound() {
+	    //given
+		MemberUpdateRequest request = memberUpdateRequest();
+		String requestUserId = "nhnacademy";
+
+		when(memberRepository.findByUserId(requestUserId)).thenReturn(Optional.empty());
+
+	    //when / then
+		MemberNotFoundException exception = assertThrows(MemberNotFoundException.class,
+			() -> memberServiceImpl.updateMember(requestUserId, request));
+		assertThat(exception.getMessage()).isEqualTo("존재하지 않는 회원입니다.");
+	}
+
+	@DisplayName("회원 정보 수정 실패 - 존재하지 않는 고객")
+	@Test
+	void updateMember_CustomerNotFound() {
+		//given
+		MemberUpdateRequest request = memberUpdateRequest();
+		Customer existingCustomer = customer();
+		Member existingMember = member(existingCustomer);
+
+		String requestUserId = "nhnacademy";
+
+		when(memberRepository.findByUserId(requestUserId)).thenReturn(Optional.of(existingMember));
+		when(customerRepository.findById(existingMember.getId())).thenReturn(Optional.empty());
+
+		//when / then
+		CustomerNotFoundException exception = assertThrows(CustomerNotFoundException.class,
+			() -> memberServiceImpl.updateMember(requestUserId, request));
+		assertThat(exception.getMessage()).isEqualTo("존재하지 않는 고객입니다.");
+	}
+
 	@DisplayName("회원 이름, 비밀번호, 권한 조회 성공")
 	@Test
-	public void checkMember() {
+	void checkMember() {
 	    //given
 		Customer existingCustomer = customer();
 		Member existingMember = member(existingCustomer);
@@ -135,6 +239,46 @@ class MemberServiceImplTest {
 		assertThat(response.getPassword()).isEqualTo(existingCustomer.getPassword());
 		assertThat(response.getAuthority()).isEqualTo("ROLE_" + existingMember.getAuthority());
 	}
+
+	@DisplayName("회원 이름, 비밀번호, 권한 조회 - 회원이 존재하지 않을 때")
+	@Test
+	void checkMember_UserIdNotFound() {
+		//given
+		String requestUserid = "nhnacademy";
+
+		when(memberRepository.findByUserId(requestUserid)).thenReturn(Optional.empty());
+
+		//when
+		MemberCheckResponse response = memberServiceImpl.checkMember(requestUserid);
+
+		//then
+		assertNotNull(response);
+		assertNull(response.getName());
+		assertNull(response.getPassword());
+		assertNull(response.getAuthority());
+	}
+
+	@DisplayName("회원 이름, 비밀번호, 권한 조회 - 고객이 존재하지 않을 때")
+	@Test
+	void checkMember_CustomerNotFound() {
+		//given
+		Customer existingCustomer = customer();
+		Member existingMember = member(existingCustomer);
+		String requestUserid = "nhnacademy";
+
+		when(memberRepository.findByUserId(requestUserid)).thenReturn(Optional.of(existingMember));
+		when(customerRepository.findById(existingMember.getId())).thenReturn(Optional.empty());
+
+		//when
+		MemberCheckResponse response = memberServiceImpl.checkMember(requestUserid);
+
+		//then
+		assertNotNull(response);
+		assertNull(response.getName());
+		assertNull(response.getPassword());
+		assertNull(response.getAuthority());
+	}
+
 
 	private MemberUpdateRequest memberUpdateRequest() {
 		return MemberUpdateRequest.builder()
