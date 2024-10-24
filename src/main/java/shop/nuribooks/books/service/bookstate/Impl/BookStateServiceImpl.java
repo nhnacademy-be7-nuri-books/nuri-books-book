9ce7f7c26d4995ddb036c63eadab7c9ddf7cbb16
ljg0610 <jggy0627@naver.com>
2024-10-24 13:43:45 +0900

    fix: 도서상태 등록 커스텀 예외처리 추가

--- src/main/java/shop/nuribooks/books/service/bookstate/Impl/BookStateServiceImpl.java
+++ src/main/java/shop/nuribooks/books/service/bookstate/Impl/BookStateServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import shop.nuribooks.books.dto.bookstate.BookStateRequest;
 import shop.nuribooks.books.entity.book.BookState;
 import shop.nuribooks.books.exception.ResourceAlreadyExistException;
+import shop.nuribooks.books.exception.bookstate.BookStateDetailAlreadyExistException;
 import shop.nuribooks.books.repository.bookstate.BookStateRepository;
 import shop.nuribooks.books.service.bookstate.BookStateService;
 
@@ -18,7 +19,7 @@ public class BookStateServiceImpl implements BookStateService {
 	@Override
 	public void registerState(String adminId, BookStateRequest bookStateRequest) {
 		if(bookStateRepository.existsBookStatesByDetail(bookStateRequest.detail())){
-			throw new ResourceAlreadyExistException("입력한 도서상태명" + bookStateRequest.detail() + "이 이미 존재합니다");
+			throw new BookStateDetailAlreadyExistException(bookStateRequest.detail());
 		}
 
 		BookState bookState = BookState.of(bookStateRequest.detail());
