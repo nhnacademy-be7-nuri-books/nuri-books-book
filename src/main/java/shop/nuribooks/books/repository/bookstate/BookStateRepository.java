957aca1966c78b551266b2fec58d116f2f0a3f43
ljg0610 <jggy0627@naver.com>
2024-10-24 10:16:18 +0900

    feature: 도서상태 repository 추가

--- src/main/java/shop/nuribooks/books/repository/bookstate/BookStateRepository.java
+++ src/main/java/shop/nuribooks/books/repository/bookstate/BookStateRepository.java
@@ -3,6 +3,8 @@ package shop.nuribooks.books.repository.bookstate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import shop.nuribooks.books.entity.book.BookState;
+import shop.nuribooks.books.entity.book.BookStateEnum;
 
-public interface BookStateRepository extends JpaRepository<BookState, Long> {
+public interface BookStateRepository extends JpaRepository<BookState, Integer> {
+	boolean existsBookStatesByDetail(BookStateEnum detail);
 }
