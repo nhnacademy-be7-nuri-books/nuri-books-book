a5e8fd7bdd1383801dd8632d3ea572c9caf5a278
kyongmin <110990435+kkyongg@users.noreply.github.com>
2024-10-24 16:50:45 +0900

    Feature/publisher get (#138)
    * feat: publisher 등록 기능 구현
    
    * fix: builder 패턴 적용 및 test code 추가
    
    * feat: response dto 메서드 추가
    
    * feat: publisher 조회 기능 구현

--- src/main/java/shop/nuribooks/books/service/publisher/PublisherService.java
+++ src/main/java/shop/nuribooks/books/service/publisher/PublisherService.java
@@ -1,9 +1,15 @@
 package shop.nuribooks.books.service.publisher;
 
+import java.util.List;
+
 import shop.nuribooks.books.dto.publisher.PublisherRequest;
 import shop.nuribooks.books.dto.publisher.PublisherResponse;
 
 public interface PublisherService {
 	//TODO: 등록, 조회, 수정, 삭제 구현
 	PublisherResponse registerPublisher(PublisherRequest request);
+
+	List<PublisherResponse> getAllPublisher();
+
+	PublisherResponse getPublisher(String name);
 }
--- src/main/java/shop/nuribooks/books/service/publisher/PublisherServiceImpl.java
+++ src/main/java/shop/nuribooks/books/service/publisher/PublisherServiceImpl.java
@@ -1,5 +1,7 @@
 package shop.nuribooks.books.service.publisher;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,6 +10,7 @@ import shop.nuribooks.books.dto.publisher.PublisherRequest;
 import shop.nuribooks.books.dto.publisher.PublisherResponse;
 import shop.nuribooks.books.entity.book.Publisher;
 import shop.nuribooks.books.exception.publisher.PublisherAlreadyExistsException;
+import shop.nuribooks.books.exception.publisher.PublisherNotFoundException;
 import shop.nuribooks.books.repository.publisher.PublisherRepository;
 
 @Service
@@ -32,5 +35,34 @@ public class PublisherServiceImpl implements PublisherService {
 		Publisher publisher = request.toEntity();
 		Publisher saved = publisherRepository.save(publisher);
 		return PublisherResponse.of(saved);
+	}
+
+	/**
+	 * getAllPublisher : 등록 되어있는 모든 출판사 정보 조회
+	 * @author kyongmin
+	 *
+	 * @return 등록된 출판사 정보를 포함한 PublisherResponse List
+	 */
+	@Override
+	public List<PublisherResponse> getAllPublisher() {
+		List<Publisher> publishers = publisherRepository.findAll().stream().toList();
+		return publishers.stream()
+			.map(PublisherResponse::of)
+			.toList();
+	}
+
+	/**
+	 * getPublisher : 출판사 정보 조회
+	 * @author kyongmin
+	 *
+	 * @param name 이름으로 출판사 정보 조회
+	 *             존재하지 않는 출판사일 경우 PublisherNotFoundException 발생
+	 * @return 해당하는 출판사 정보를 포함한 PublisherResponse
+	 */
+	@Override
+	public PublisherResponse getPublisher(String name) {
+		Publisher publisher = publisherRepository.findByName(name)
+			.orElseThrow(() -> new PublisherNotFoundException("출판사가 존재하지 않습니다."));
+		return PublisherResponse.of(publisher);
 	}
 }
