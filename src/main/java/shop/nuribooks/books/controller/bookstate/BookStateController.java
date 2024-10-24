1126a3440518359f11a517752ae009ad1e3678bf
ljg0610 <jggy0627@naver.com>
2024-10-24 22:57:45 +0900

    feature: 도서상태 수정 컨트롤러 메서드 추가

--- src/main/java/shop/nuribooks/books/controller/bookstate/BookStateController.java
+++ src/main/java/shop/nuribooks/books/controller/bookstate/BookStateController.java
@@ -6,6 +6,8 @@ import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -51,4 +53,22 @@ public class BookStateController {
 		List<BookStateResponse> bookStateResponses = bookStateService.getAllBooks();
 		return ResponseEntity.ok(bookStateResponses);
 	}
+
+	@PatchMapping("/{id}")
+	@Operation(summary = "도서 상태 수정", description = "관리자가 도서 상태를 수정합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "도서 상태 수정 성공"),
+		@ApiResponse(responseCode = "400", description = "잘못된 요청 데이터"),
+		@ApiResponse(responseCode = "404", description = "도서 상태를 찾을 수 없음"),
+		@ApiResponse(responseCode = "409", description = "도서 상태가 이미 존재함"),
+	})
+	public ResponseEntity<ResponseMessage> updateBookState(
+		@PathVariable Integer id,
+		@Valid @RequestBody BookStateRequest bookStateReq) {
+
+		bookStateService.updateState(id, bookStateReq);
+		ResponseMessage responseMessage = new ResponseMessage(HttpStatus.OK.value(), "도서상태 수정 성공");
+		return ResponseEntity.ok(responseMessage);
+	}
+
 }
