ee286b2bce7ee8e9c7d827c7b59263bdd7b8ed4d
jaaaayk <janghyun30@gmail.com>
2024-10-28 13:40:39 +0900

    fix: 주석추가

--- src/main/java/shop/nuribooks/books/book/category/service/impl/BookCategoryServiceImpl.java
+++ src/main/java/shop/nuribooks/books/book/category/service/impl/BookCategoryServiceImpl.java
@@ -13,6 +13,9 @@ import shop.nuribooks.books.book.category.service.BookCategoryService;
 import shop.nuribooks.books.exception.book.BookNotFoundException;
 import shop.nuribooks.books.exception.category.CategoryNotFoundException;
 
+/**
+ * 도서와 카테고리 간의 연관 관계를 관리하는 서비스 구현 클래스입니다.
+ */
 @RequiredArgsConstructor
 @Service
 public class BookCategoryServiceImpl implements BookCategoryService {
@@ -20,6 +23,15 @@ public class BookCategoryServiceImpl implements BookCategoryService {
 	private final CategoryRepository categoryRepository;
 	private final BookCategoryRepository bookCategoryRepository;
 
+	/**
+	 * 도서 ID와 카테고리 ID를 받아 두 객체를 연관시킵니다.
+	 *
+	 * @author janghyun
+	 * @param bookId     연관시킬 도서의 ID
+	 * @param categoryId 연관시킬 카테고리의 ID
+	 * @throws BookNotFoundException     해당 ID의 도서를 찾을 수 없을 때 발생합니다.
+	 * @throws CategoryNotFoundException 해당 ID의 카테고리를 찾을 수 없을 때 발생합니다.
+	 */
 	@Override
 	public void registerBookCategory(Long bookId, Long categoryId) {
 		Book book = bookRepository.findById(bookId)
