902462bc1f11832cd626ec026e115fa3c1db4022
ljg0610 <jggy0627@naver.com>
2024-10-28 11:22:00 +0900

    feat: 도서상세조회 컨트롤러 메서드 추가

--- src/main/java/shop/nuribooks/books/book/book/controller/BookController.java
+++ src/main/java/shop/nuribooks/books/book/book/controller/BookController.java
@@ -3,6 +3,7 @@ package shop.nuribooks.books.book.book.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,6 +18,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import shop.nuribooks.books.book.book.dto.BookRegisterRequest;
 import shop.nuribooks.books.book.book.dto.BookRegisterResponse;
+import shop.nuribooks.books.book.book.dto.BookResponse;
 import shop.nuribooks.books.book.book.dto.BookUpdateRequest;
 import shop.nuribooks.books.book.book.service.BookService;
 import shop.nuribooks.books.common.message.ResponseMessage;
@@ -38,6 +40,17 @@ public class BookController {
 	public ResponseEntity<BookRegisterResponse> registerBooks(@Valid @RequestBody BookRegisterRequest reqDto) {
 		BookRegisterResponse resDto = bookService.registerBook(reqDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(resDto);
+	}
+
+	@Operation(summary = "도서 상세 조회", description = "도서 ID를 통해 도서의 상세 정보를 조회하는 엔드포인트입니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "도서 조회 성공"),
+		@ApiResponse(responseCode = "404", description = "존재하지 않는 도서입니다.")
+	})
+	@GetMapping("/{bookId}")
+	public ResponseEntity<BookResponse> getBookById(@PathVariable Long bookId) {
+		BookResponse bookResponse = bookService.getBookById(bookId);
+		return ResponseEntity.status(HttpStatus.OK).body(bookResponse);
 	}
 
 	@Operation(summary = "도서 정보 수정", description = "관리자가 도서 ID를 통해 기존 도서의 정보를 수정할 수 있는 엔드포인트입니다.")
