0124a804b3d140a49090dc77e91564de2ac69bf7
kkyongg <kkyongg2@gmail.com>
2024-10-29 09:37:49 +0900

    feat: tag 조회 기능 구현

--- src/main/java/shop/nuribooks/books/book/tag/service/TagServiceImpl.java
+++ src/main/java/shop/nuribooks/books/book/tag/service/TagServiceImpl.java
@@ -1,5 +1,7 @@
 package shop.nuribooks.books.book.tag.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import shop.nuribooks.books.book.tag.dto.TagResponse;
 import shop.nuribooks.books.book.tag.entity.Tag;
 import shop.nuribooks.books.book.tag.repository.TagRepository;
 import shop.nuribooks.books.exception.tag.TagAlreadyExistsException;
+import shop.nuribooks.books.exception.tag.TagNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -17,7 +20,6 @@ public class TagServiceImpl implements TagService {
 
 	/**
 	 * registerTag : 태그 등록
-	 * @author kyongmin
 	 *
 	 * @param request 태그 이름 등록
 	 *                이미 존재하는 태그일생경우 TagAlreadyExistsException 발생
@@ -33,4 +35,34 @@ public class TagServiceImpl implements TagService {
 
 		return TagResponse.of(savedTag);
 	}
+
+
+	/**
+	 * getAllTags : 등록 되어있는 모든 태그 조회
+	 *
+	 * @return 등록된 태그 정보를 포함한 TagResponse List
+	 */
+	@Override
+	public List<TagResponse> getAllTags() {
+		List<Tag> tags = tagRepository.findAll().stream().toList();
+		return tags.stream()
+			.map(TagResponse::of)
+			.toList();
+	}
+
+	/**
+	 * getTag : 태그 조회
+	 *
+	 * @param id id로 태그 정보 조회
+	 *           존재하지 않는 태그일 경우 TagNotFoundException 발생
+	 * @return id에 해당하는 태그 정보 TagResponse
+	 */
+	@Override
+	public TagResponse getTag(Long id) {
+		Tag tag = tagRepository.findById(id)
+			.orElseThrow(() -> new TagNotFoundException("태그가 존재하지 않습니다."));
+
+		return TagResponse.of(tag);
+	}
+
 }
