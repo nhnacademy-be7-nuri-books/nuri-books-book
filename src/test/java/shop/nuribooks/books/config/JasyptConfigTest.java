59b8a085218182323f09e62a1268b0b8787133ec
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-10-29 14:58:27 +0900

    feat: jasypt db 복호화 및 암호화 config

---
+++ src/test/java/shop/nuribooks/books/config/JasyptConfigTest.java
@@ -0,0 +1,36 @@
+package shop.nuribooks.books.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+public class JasyptConfigTest {
+	@Autowired
+	@Qualifier("jasyptStringEncryptor")
+	StringEncryptor encryptor;
+	@Autowired
+	private Environment env;
+
+	@Test
+	void test() {
+		encryptInfos("MYSQL_URL");
+		encryptInfos("MYSQL_DEV_URL");
+		encryptInfos("MYSQL_ID");
+		encryptInfos("MYSQL_PWD");
+	}
+
+	private String encryptInfos(String key) {
+		String value = env.getProperty(key);
+		String encVal = encryptor.encrypt(value);
+		log.info("{} : {}", key, encVal);
+		log.info("{}", encryptor.decrypt(encVal));
+		return encVal;
+	}
+}
