d94457be56642e3cb1628000a71740c025473c9f
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-10-31 01:25:06 +0900

    feat: review관련 entity 작성.

---
+++ src/main/java/shop/nuribooks/books/book/review/entity/Review.java
@@ -0,0 +1,82 @@
+package shop.nuribooks.books.book.review.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import shop.nuribooks.books.book.book.entitiy.Book;
+import shop.nuribooks.books.member.member.entity.Member;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "reviews")
+public class Review {
+	@Id
+	@Column(name = "review_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	private String title;
+
+	@NotNull
+	private String content;
+
+	@NotNull
+	@ColumnDefault("0")
+	@Min(value = 0, message = "별점은 0 이상이어야합니다.")
+	@Max(value = 5, message = "별점은 5 이하여야합니다.")
+	@Column(nullable = false)
+	//@Column(nullable = false, columnDefinition = "tinyint(1)")
+	private int score; //평점 1-5점
+
+	@NotNull
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updateAt;
+
+	@ManyToOne
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@ManyToOne
+	@JoinColumn(name = "book_id", nullable = false)
+	private Book book;
+
+	// TODO:: 상세 주문 필드 추가
+
+	// 조회 시 사용될듯?
+	@OneToMany(mappedBy = "review")
+	private List<ReviewImage> reviewImage;
+
+	@Builder
+	public Review(String title, String content, int score, Member member, Book book) {
+		this.title = title;
+		this.content = content;
+		this.score = score;
+		this.createdAt = LocalDateTime.now();
+		this.member = member;
+		this.book = book;
+	}
+}
--- src/main/java/shop/nuribooks/books/book/review/entity/ReviewImage.java
+++ src/main/java/shop/nuribooks/books/book/review/entity/ReviewImage.java
@@ -1,12 +1,16 @@
-package shop.nuribooks.books.book.book.entitiy;
+package shop.nuribooks.books.book.review.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,8 +24,19 @@ import lombok.Setter;
 public class ReviewImage {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "review_image_id")
 	private Long id;
 
 	@NotNull
 	private String imageUrl;
+
+	@ManyToOne
+	@JoinColumn(name = "review_id", nullable = false)
+	private Review review;
+
+	@Builder
+	public ReviewImage(String imageUrl, Review review) {
+		this.imageUrl = imageUrl;
+		this.review = review;
+	}
 }
