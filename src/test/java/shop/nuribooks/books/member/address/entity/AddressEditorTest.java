5a81f69ed49d2fe2a224bbb004a9d07ab06b2259
hxcva1 <hxcva1@gmail.com>
2024-10-31 13:44:45 +0900

    fix: DTO record로 변경

---
+++ src/test/java/shop/nuribooks/books/member/address/entity/AddressEditorTest.java
@@ -0,0 +1,84 @@
+package shop.nuribooks.books.member.address.entity;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import shop.nuribooks.books.member.address.dto.requset.AddressEditRequest;
+import shop.nuribooks.books.member.address.entity.AddressEditor.AddressEditorBuilder;
+
+class AddressEditorTest {
+
+    @DisplayName("Editor 테스트")
+    @Test
+    void test() {
+        // given
+        Address address = Address.builder()
+                .name("name")
+                .address("address")
+                .addressDetail("addressDetail")
+                .isDefault(false)
+                .build();
+
+        AddressEditRequest request = AddressEditRequest.builder()
+                .id(1L)
+                .name("name2")
+                .address(null)
+                .addressDetail(null)
+                .isDefault(true)
+                .build();
+
+        AddressEditorBuilder addressEditorBuilder = address.toEditor();
+        AddressEditor addressEditor = addressEditorBuilder
+                .name(request.name())
+                .address(request.address())
+                .addressDetail(request.addressDetail())
+                .isDefault(request.isDefault())
+                .build();
+
+        // when
+        address.edit(addressEditor);
+
+        // then
+        assertThat(address).extracting("name", "address", "addressDetail", "isDefault")
+                .containsExactly("name2", "address", "addressDetail", true);
+    }
+
+    @DisplayName("Address 빌더 테스트")
+    @Test
+    void addressBuilderTest() {
+
+        // when
+        Address address = Address.builder()
+                .name("name")
+                .address("address")
+                .addressDetail("addressDetail")
+                .isDefault(false)
+                .build();
+
+        // then
+        assertThat(address).extracting("name", "address", "addressDetail", "isDefault")
+                .containsExactly("name", "address", "addressDetail", false);
+    }
+//
+//
+//    @DisplayName("AddressEditRequest 빌더 테스트")
+//    @Test
+//    void addressBuilderTest() {
+//
+//        // when
+//        Address address = Address.builder()
+//                .name("name")
+//                .address("address")
+//                .addressDetail("addressDetail")
+//                .isDefault(false)
+//                .build();
+//
+//        // then
+//        assertThat(address).extracting("name", "address", "addressDetail", "isDefault")
+//                .containsExactly("name", "address", "addressDetail", false);
+//    }
+}
