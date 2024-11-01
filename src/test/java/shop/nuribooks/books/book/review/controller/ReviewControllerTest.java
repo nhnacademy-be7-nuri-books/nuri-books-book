61aae450c4e99b2757c2cef59a55c8ef8e9d62f1
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-11-01 22:56:02 +0900

    feat: controller 테스트 완료.

---
+++ src/test/java/shop/nuribooks/books/book/review/controller/ReviewControllerTest.java
@@ -0,0 +1,62 @@
+package shop.nuribooks.books.book.review.controller;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.nuribooks.books.book.review.dto.request.ReviewRegisterRequest;
+import shop.nuribooks.books.book.review.dto.response.ReviewBreifResponse;
+import shop.nuribooks.books.book.review.service.ReviewService;
+
+@WebMvcTest(ReviewController.class)
+public class ReviewControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private ReviewService reviewService;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	void registerTest() throws Exception {
+		ReviewRegisterRequest reviewRequest = new ReviewRegisterRequest(
+			"title",
+			"contentcontent",
+			4,
+			1L,
+			List.of("http://example.com/image1.jpg", "http://example.com/image2.jpg")
+		);
+
+		ReviewBreifResponse response = new ReviewBreifResponse(
+			0,
+			"title",
+			"contentcontent",
+			4,
+			null
+		);
+
+		when(reviewService.registerReview(any(ReviewRegisterRequest.class), eq(1L))).thenReturn(response);
+
+		// Act & Assert
+		mockMvc.perform(post("/api/reviews")
+				.header("X-USER-ID", "1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(reviewRequest)))
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.title").value("title"));
+	}
+}
+
