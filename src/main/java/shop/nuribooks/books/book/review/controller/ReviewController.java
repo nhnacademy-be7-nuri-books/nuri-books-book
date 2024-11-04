cc779278411c515db641f3010a81c7c3a83cf277
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2024-11-04 13:24:22 +0900

    Feature/review create (#195)
    * feat: review관련 entity 작성.
    
    * feat: review register response dto 생성.
    
    * fix: review image 리스트 로직 수정.
    
    * feat: review image dto 추가.
    
    * feat: review repository.
    
    * feat: 리뷰 응답 dto 생성.
    
    * feat: review create service 구현.
    
    * feat: 리뷰 이미지 예외 처리.
    
    * feat: 리뷰 이미지 갯수 초과 예외
    
    * feat: register service test 완료.
    
    * feat: 주석 추가.
    
    * feat: 리뷰 등록 컨트롤러.
    
    * fix: 리뷰 이미지 로직 수정.
    
    * feat: controller 테스트 완료.
    
    * fix: merge bug fix.
    
    * fix: 각종 수정.
    
    * feat: review image cascade.
    
    * feat: member brief response dto 생성.
    
    * fix: entity 수정.
    
    * fix: notnull -> notblank.

---
+++ src/main/java/shop/nuribooks/books/book/review/controller/ReviewController.java
@@ -0,0 +1,39 @@
+package shop.nuribooks.books.book.review.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import shop.nuribooks.books.book.review.dto.request.ReviewRegisterRequest;
+import shop.nuribooks.books.book.review.dto.response.ReviewMemberResponse;
+import shop.nuribooks.books.book.review.service.ReviewService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/reviews")
+public class ReviewController {
+	private final ReviewService reviewService;
+
+	@Operation(summary = "리뷰 등록", description = "새로운 리뷰를 등록합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "201", description = "리뷰 생성 성공"),
+		@ApiResponse(responseCode = "400", description = "잘못된 요청 데이터"),
+	})
+	@PostMapping
+	public ResponseEntity<ReviewMemberResponse> registerReview(
+		@Valid @RequestBody ReviewRegisterRequest reviewRegisterRequest,
+		@RequestHeader("X-USER-ID") long memberId
+	) {
+		ReviewMemberResponse response = this.reviewService.registerReview(reviewRegisterRequest, memberId);
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+}
