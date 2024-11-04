c884a1abe1c30343e87048c14adf8fd9b5c123c6
Jprotection <bigho98@gmail.com>
2024-11-04 17:29:17 +0900

    feat: 회원 목록 조회 및 탈퇴 회원 스케줄링 구현

--- src/main/java/shop/nuribooks/books/member/member/service/MemberServiceImpl.java
+++ src/main/java/shop/nuribooks/books/member/member/service/MemberServiceImpl.java
@@ -3,6 +3,8 @@ package shop.nuribooks.books.member.member.service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,18 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import shop.nuribooks.books.exception.member.GradeNotFoundException;
-import shop.nuribooks.books.member.address.repository.AddressRepository;
 import shop.nuribooks.books.member.grade.entity.Grade;
 import shop.nuribooks.books.member.grade.repository.GradeRepository;
 import shop.nuribooks.books.member.member.dto.DtoMapper;
 import shop.nuribooks.books.member.member.dto.EntityMapper;
 import shop.nuribooks.books.member.member.dto.request.MemberDetailsRequest;
 import shop.nuribooks.books.member.member.dto.request.MemberRegisterRequest;
+import shop.nuribooks.books.member.member.dto.request.MemberSearchRequest;
 import shop.nuribooks.books.member.member.dto.request.MemberUpdateRequest;
 import shop.nuribooks.books.member.member.dto.request.MemberWithdrawRequest;
 import shop.nuribooks.books.member.member.dto.response.MemberAuthInfoResponse;
 import shop.nuribooks.books.member.member.dto.response.MemberDetailsResponse;
 import shop.nuribooks.books.member.member.dto.response.MemberRegisterResponse;
+import shop.nuribooks.books.member.member.dto.response.MemberSearchResponse;
 import shop.nuribooks.books.member.member.dto.response.MemberUpdateResponse;
 import shop.nuribooks.books.member.customer.entity.Customer;
 import shop.nuribooks.books.member.member.entity.AuthorityType;
@@ -35,7 +38,6 @@ import shop.nuribooks.books.exception.member.UserIdNotFoundException;
 import shop.nuribooks.books.member.customer.repository.CustomerRepository;
 import shop.nuribooks.books.member.member.entity.StatusType;
 import shop.nuribooks.books.member.member.repository.MemberRepository;
-import shop.nuribooks.books.member.resignedmember.repository.ResignedMemberRepository;
 
 /**
  * @author Jprotection
@@ -49,8 +51,6 @@ public class MemberServiceImpl implements MemberService {
 	private final CustomerRepository customerRepository;
 	private final MemberRepository memberRepository;
 	private final GradeRepository gradeRepository;
-	private final ResignedMemberRepository resignedMemberRepository;
-	private final AddressRepository addressRepository;
 
 	/**
 	 * 회원등록 <br>
@@ -64,6 +64,7 @@ public class MemberServiceImpl implements MemberService {
 	 * 권한은 MEMBER, 등급은 STANDARD, 상태는 ACTIVE로 초기화 <br>
 	 * @return MemberRegisterResponse에 이름, 성별, 유저 아이디, 전화번호, 이메일, 생일을 담아서 반환
 	 */
+	@Override
 	@Transactional
 	public MemberRegisterResponse registerMember(MemberRegisterRequest request) {
 		if (customerRepository.existsByEmail(request.email())) {
@@ -106,6 +107,7 @@ public class MemberServiceImpl implements MemberService {
 	 * customer까지 존재한다면 마지막으로 member의 status를 INACTIVE로, <br>
 	 * 탈퇴 일시인 withdrawnAt을 현재 시간으로 변경
 	 */
+	@Override
 	@Transactional
 	public void withdrawMember(MemberWithdrawRequest request) {
 		Member foundMember = memberRepository.findByUserId(request.userId())
@@ -132,18 +134,19 @@ public class MemberServiceImpl implements MemberService {
 	 * 입력받은 각각의 이름, 비밀번호, 전화번호로 customer의 정보 수정
 	 * @return MemberUpdateResponse에 변경한 이름과 전화번호 담아서 반환
 	 */
+	@Override
 	@Transactional
 	public MemberUpdateResponse updateMember(String userId, MemberUpdateRequest request) {
 		Member foundMember = memberRepository.findByUserId(userId)
 			.orElseThrow(() -> new MemberNotFoundException("존재하지 않는 회원입니다."));
 
-		Customer findCustomer = customerRepository.findById(foundMember.getId())
+		Customer foundCustomer = customerRepository.findById(foundMember.getId())
 			.orElseThrow(() -> new CustomerNotFoundException("존재하지 않는 고객입니다."));
 
-		findCustomer.changeCustomerInformation(
+		foundCustomer.changeCustomerInformation(
 			request.name(), request.password(), request.phoneNumber());
 
-		return DtoMapper.toUpdateDto(findCustomer);
+		return DtoMapper.toUpdateDto(foundCustomer);
 	}
 
 	/**
@@ -152,6 +155,7 @@ public class MemberServiceImpl implements MemberService {
 	 * @return 회원이 존재하면 이름, 비밀번호, 권한을 MemberAuthInfoResponse에 담아서 반환 <br>
 	 * 회원이 존재하지 않는다면 이름, 비밀번호, 권한이 모두 null인 MemberAuthInfoResponse를 반환
 	 */
+	@Override
 	public MemberAuthInfoResponse getMemberAuthInfo(String userId) {
 
 		return memberRepository.findByUserId(userId)
@@ -168,6 +172,7 @@ public class MemberServiceImpl implements MemberService {
 	 * @param request MemberDetailsRequest로 상세 조회하고 싶은 userId와 password를 받음
 	 * @return MemberDetailsResponse에 회원의 모든 정보를 담아서 반환
 	 */
+	@Override
 	public MemberDetailsResponse getMemberDetails(MemberDetailsRequest request) {
 
 		Member foundMember = memberRepository.findByUserId(request.userId())
@@ -177,6 +182,14 @@ public class MemberServiceImpl implements MemberService {
 			.orElseThrow(() -> new InvalidPasswordException("비밀번호가 일치하지 않습니다."));
 
 		return DtoMapper.toDetailsDto(foundCustomer, foundMember);
+	}
+
+	/**
+	 * 관리자가 다양한 검색 조건을 이용하여 회원 목록 조회
+	 */
+	@Override
+	public Page<MemberSearchResponse> searchMembersWithPaing(MemberSearchRequest request, Pageable pageable) {
+		return memberRepository.searchMembersWithPaging(request, pageable);
 	}
 
 	/**
@@ -190,58 +203,35 @@ public class MemberServiceImpl implements MemberService {
 
 		memberRepository.findAllByLatestLoginAtBefore(thresholdDate)
 			.stream()
-			.filter(member -> member.getStatus() == StatusType.ACTIVE)
-			.forEach(member -> {
-				log.info("신규 휴면 회원이 발생하였습니다. - 회원 아이디: {}", member.getUserId());
-				member.changeToInactive();
+			.filter(foundMember -> foundMember.getStatus() == StatusType.ACTIVE)
+			.forEach(foundMember -> {
+				log.info("신규 휴면 회원이 발생하였습니다. - 회원 아이디: {}", foundMember.getUserId());
+
+				foundMember.changeToInactive();
 			});
 	}
 
-	// /**
-	//  * @deprecated 이 스케쥴링 메서드는 사용되지 않을 것입니다.
-	//  * 탈퇴 회원의 Withdrawn상태 기간 경과를 매일 자정에 확인하여 1년이 지나면 <br>
-	//  * 해당 회원들의 주소를 모두 찾아 AddressRepository에서 삭제하고, <br>
-	//  * MemberRepository에서 해당 회원을, CustomerRepository에서도 동일한 id의 비회원을 삭제한다. <br>
-	//  * 그 회원이 사용했던 userId만을 ResignedMember에 담아 ResignedMemberRepository에 저장
-	//  */
-	// @Deprecated
-	// @Scheduled(cron = "0 0 0 * * ?")
-	// @Transactional
-	// public void removeWithdrawnMembers() {
-	// 	try {
-	// 		List<Member> membersToDelete = memberRepository.findAll().stream()
-	// 			.filter(Member::isWithdrawnForOverOneYear)
-	// 			.toList();
-	//
-	// 		if (membersToDelete.isEmpty()) {
-	// 			log.info("탈퇴 일시가 1년이 지난 회원이 존재하지 않습니다.");
-	// 			return;
-	// 		}
-	//
-	// 		List<Customer> customersToDelete = membersToDelete.stream()
-	// 			.map(member -> customerRepository.findById(member.getId())
-	// 				.orElseThrow(() -> new CustomerNotFoundException("삭제하려는 고객이 존재하지 않습니다.")))
-	// 			.toList();
-	//
-	// 		List<Address> addressesToDelete = membersToDelete.stream()
-	// 			.flatMap(member -> addressRepository.findAllByMemberId(member.getId()).stream())
-	// 			.toList();
-	//
-	// 		List<ResignedMember> completelyResignedMembers = membersToDelete.stream()
-	// 			.map(member -> ResignedMember.builder()
-	// 				.resignedUserId(member.getUserId())
-	// 				.build())
-	// 			.toList();
-	//
-	// 		addressRepository.deleteAll(addressesToDelete);
-	// 		memberRepository.deleteAll(membersToDelete);
-	// 		customerRepository.deleteAll(customersToDelete);
-	// 		resignedMemberRepository.saveAll(completelyResignedMembers);
-	//
-	// 	} catch (Exception e) {
-	// 		log.error("회원의 탈퇴 일시 경과를 확인하는 스케줄링에 에러가 발생했습니다 : {}", e.getMessage());
-	// 	}
-	// }
+	/**
+	 * 매일 04:30분에 탈퇴 날짜가 1년이 지난 회원들을 찾아, <br>
+	 * userId와, status, withdrawnAt을 제외한 모든 필드들을 공백 처리
+	 */
+	@Scheduled(cron = "0 30 4 * * ?")
+	@Transactional
+	public void removeWithdrawnMembers() {
+		LocalDateTime thresholdDate = LocalDateTime.now().minusYears(1);
+
+		memberRepository.findAllByWithdrawnAtBefore(thresholdDate)
+			.forEach(foundMember -> {
+				log.info("탈퇴 후 1년이 지나 SOFT DELETE의 대상이 되는 회원을 발견했습니다. - 회원 아이디: {}",
+					foundMember.getUserId());
+
+				Customer foundCustomer = customerRepository.findById(foundMember.getId())
+					.orElseThrow(() -> new CustomerNotFoundException("존재하지 않는 고객입니다."));
+
+				foundCustomer.changeToSoftDeleted();
+				foundMember.changeToSoftDeleted();
+			});
+	}
 
 	/**
 	 * STANDARD 등급을 가져오는 메서드
