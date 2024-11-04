52d73f8b13b5126c17913fea877c32b8f23b581c
ljg0610 <jggy0627@naver.com>
2024-11-04 11:23:28 +0900

    feat: 알라딘 api이용 도서 저장 컨트롤러 메서드 1차 구현

--- src/main/java/shop/nuribooks/books/book/book/controller/AladinBookController.java
+++ src/main/java/shop/nuribooks/books/book/book/controller/AladinBookController.java
@@ -2,17 +2,22 @@ package shop.nuribooks.books.book.book.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import shop.nuribooks.books.book.book.dto.AladinBookListItemResponse;
-import shop.nuribooks.books.book.book.dto.AladinBookListResponse;
+import shop.nuribooks.books.book.book.dto.AladinBookSaveRequest;
+import shop.nuribooks.books.book.book.dto.BookResponse;
 import shop.nuribooks.books.book.book.service.AladinBookService;
 
 @RequestMapping("api/books/aladin")
@@ -31,5 +36,11 @@ public class AladinBookController {
 	public ResponseEntity<List<AladinBookListItemResponse>> getAladinBookList() {
 		List<AladinBookListItemResponse> books = aladinBookService.getNewBooks();
 		return ResponseEntity.ok(books);
+	}
+
+	@PostMapping("/save")
+	public ResponseEntity<BookResponse> saveAladinBook(@Valid @RequestBody AladinBookSaveRequest aladinBookSaveReq) {
+		BookResponse bookResponse = aladinBookService.saveBook(aladinBookSaveReq);
+		return ResponseEntity.status(HttpStatus.CREATED).body(bookResponse);
 	}
 }
