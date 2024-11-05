618aeb8ad1cb01c2c24c8edf7ea0a40983ebbb7b
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-11-05 13:52:50 +0900

    fix: removed unused method.

--- src/main/java/shop/nuribooks/books/book/book/dto/BookBriefResponse.java
+++ src/main/java/shop/nuribooks/books/book/book/dto/BookBriefResponse.java
@@ -1,18 +1,9 @@
 package shop.nuribooks.books.book.book.dto;
 
 import lombok.Builder;
-import shop.nuribooks.books.book.book.entitiy.Book;
 
 @Builder
 public record BookBriefResponse(Long id,
 								String title,
 								String thumbnailImageUrl) {
-
-	public static BookBriefResponse of(Book book) {
-		return BookBriefResponse.builder()
-			.id(book.getId())
-			.title(book.getTitle())
-			.thumbnailImageUrl(book.getThumbnailImageUrl())
-			.build();
-	}
 }
