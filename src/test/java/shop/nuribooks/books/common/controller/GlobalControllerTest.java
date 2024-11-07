e74fa1a3e006e5d86dca496e772fdb8aa4fec20b
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2024-11-07 14:45:09 +0900

    Feature/nonstop deploy (#211)
    * feat: 서버 셧다운.
    
    * feat: test controller.
    
    * feat: production deploy 업데이트
    
    * fix: fixed typo

---
+++ src/test/java/shop/nuribooks/books/common/controller/GlobalControllerTest.java
@@ -0,0 +1,27 @@
+package shop.nuribooks.books.common.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+
+@WebMvcTest(GlobalController.class)
+public class GlobalControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private ApplicationInfoManager applicationInfoManager;
+
+	@Test
+	public void test() throws Exception {
+		mockMvc.perform(post("/actuator/shutdown"))
+			.andExpect(status().isOk());
+	}
+}
