f6fea0a54187c3f03d9ba40b808ca8556a4d1168
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-11-09 16:50:46 +0900

    feat: create new queries.

--- src/main/java/shop/nuribooks/books/book/point/repository/PointPolicyRepository.java
+++ src/main/java/shop/nuribooks/books/book/point/repository/PointPolicyRepository.java
@@ -1,8 +1,16 @@
 package shop.nuribooks.books.book.point.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import shop.nuribooks.books.book.point.dto.response.PointPolicyResponse;
 import shop.nuribooks.books.book.point.entity.PointPolicy;
 
 public interface PointPolicyRepository extends JpaRepository<PointPolicy, Long> {
+	List<PointPolicyResponse> findAllByDeletedAtIsNull();
+
+	PointPolicyResponse findPointPolicyByNameIgnoreCaseAndDeletedAtIsNull(String name);
+
+	Boolean existsByNameIgnoreCaseAndDeletedAtIsNull(String name);
 }
