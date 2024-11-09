044d1b1e0c07f1512f0e15abc7ad71d6aab3efa7
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-11-09 15:55:24 +0900

    feat: point entities.

---
+++ src/main/java/shop/nuribooks/books/book/point/entity/PointHistory.java
@@ -0,0 +1,47 @@
+package shop.nuribooks.books.book.point.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.nuribooks.books.member.member.entity.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "point_histories")
+public class PointHistory {
+	@Id
+	@Column(name = "point_history_id")
+	@GeneratedValue
+	private Long id;
+
+	@NotNull
+	@Min(0)
+	private BigDecimal amount;
+
+	private String description;
+
+	@NotNull
+	private LocalDateTime createdAt;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "customer_id", nullable = false)
+	private Member member;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "point_policy_id", nullable = false)
+	private PointPolicy pointPolicy;
+}
---
+++ src/main/java/shop/nuribooks/books/book/point/entity/PointPolicy.java
@@ -0,0 +1,42 @@
+package shop.nuribooks.books.book.point.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.nuribooks.books.book.point.enums.PolicyType;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "point_policies")
+public class PointPolicy {
+	@Id
+	@Column(name = "point_policy_id")
+	@GeneratedValue
+	private int id;
+
+	@NotNull
+	private PolicyType policyType;
+
+	@NotNull
+	private String name;
+
+	// 포인트 적용량 값. type이 fixed -> 그냥 적용 값. rated -> 적용 비율
+	@NotNull
+	@Min(0)
+	private BigDecimal amount;
+
+	@NotNull
+	private LocalDateTime createdAt;
+
+	private LocalDateTime deletedAt;
+}
