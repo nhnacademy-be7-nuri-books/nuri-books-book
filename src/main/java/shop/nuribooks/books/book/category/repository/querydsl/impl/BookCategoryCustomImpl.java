ea4cc80fb88c9d350b7b216f8e83f5de2cf15f14
jaaaayk <janghyun30@gmail.com>
2024-11-10 15:50:55 +0900

    feature: bookid 로 카테고리 리스트 조회하는 쿼리메소드 생성

---
+++ src/main/java/shop/nuribooks/books/book/category/repository/querydsl/impl/BookCategoryCustomImpl.java
@@ -0,0 +1,38 @@
+package shop.nuribooks.books.book.category.repository.querydsl.impl;
+
+import static shop.nuribooks.books.book.category.entity.QBookCategory.*;
+import static shop.nuribooks.books.book.category.entity.QCategory.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+import shop.nuribooks.books.book.category.dto.SimpleCategoryResponse;
+import shop.nuribooks.books.book.category.entity.Category;
+import shop.nuribooks.books.book.category.repository.querydsl.BookCategoryCustom;
+
+@RequiredArgsConstructor
+public class BookCategoryCustomImpl implements BookCategoryCustom {
+
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public List<List<SimpleCategoryResponse>> findCategoriesByBookId(Long bookId) {
+		List<Category> categories = queryFactory.select(category)
+			.from(bookCategory)
+			.join(bookCategory.category, category).fetchJoin()
+			.where(bookCategory.book.id.eq(bookId))
+			.fetch();
+
+		List<List<SimpleCategoryResponse>> breadcrumbsList = new ArrayList<>();
+
+		for (Category category : categories) {
+			List<SimpleCategoryResponse> breadcrumbs = SimpleCategoryResponse.buildBreadcrumbs(category);
+			breadcrumbsList.add(breadcrumbs);
+		}
+
+		return breadcrumbsList;
+	}
+}
