a3aea283a248e1fb4dcab8cb042b464c06aeaece
ljg0610 <jggy0627@naver.com>
2024-11-15 11:18:38 +0900

    Feature/config 이미지매니저 이용한 이미지업로드 기능 구현 (#273)
    * feat: dbcp2 설정
    
    * feat: 키매니저, 이미지 매니저 설정
    
    * feat: 키매니저 데이터 조회용 클래스 추가
    
    * feat: 파일업로드를 위한 image manager 서비스 구현
    
    * feat: 파일업로드 컨트롤러 구현
    
    * feat: image manager 기능 추가
    
    * feat: 이미지 업로드 엔드포인트 스웨거 추가
    
    * feat: 키매니저, 이미지매니저 시크릿 키 환경변수 설정
    
    * feat: 테스트 사용 시 필요한 환경변수 설정
    
    * fix: 워크플로우 설정 변경

---
+++ src/main/java/shop/nuribooks/books/common/Image/ImageManagerService.java
@@ -0,0 +1,76 @@
+package shop.nuribooks.books.common.Image;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import shop.nuribooks.books.common.config.keymanager.KeyManagerConfig;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ImageManagerService {
+	private final KeyManagerConfig keyManagerConfig;
+	private final RestTemplate restTemplate;
+	private final ObjectMapper objectMapper;
+
+	@Value("${cloud.nhn.image-manager.url}")
+	private String imageManagerUrl;
+
+	@Value("${cloud.nhn.image-manager.app-key}")
+	private String imageAppKey;
+
+	@Value("${cloud.nhn.image-manager.secret-id}")
+	private String imageSecretId;
+
+	public String uploadImage(MultipartFile file) throws IOException {
+		String originalFileName = file.getOriginalFilename();
+		if(originalFileName == null || originalFileName.isEmpty()) {
+			originalFileName = "default_image.png";
+		}
+
+		String encodedFileName = URLEncoder.encode(originalFileName, StandardCharsets.UTF_8);
+
+		String path = "/nuribooks/" + encodedFileName;
+		String uploadUrl = imageManagerUrl + "/" + imageAppKey + "/images?path=" + path + "&overwrite=true";
+
+		String secretKey = keyManagerConfig.getSecretValue(imageSecretId);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", secretKey);
+		headers.set("Content-Type", "application/octet-stream");
+
+		//파일 데이터 설정 및 헤더 구성
+		HttpEntity<byte[]> requestEntity = new HttpEntity<>(file.getBytes(), headers);
+
+		//이미지 업로드 요청
+		ResponseEntity<String> response = restTemplate.exchange(uploadUrl, HttpMethod.PUT, requestEntity, String.class);
+
+		if(response.getStatusCode().is2xxSuccessful()) {
+			//JSON 응답 파싱하여 URL 추출
+			log.info("{}", response.getBody());
+			JsonNode root = objectMapper.readTree(response.getBody());
+			String imageUrl = root.path("file").path("url").asText();
+			log.info("Image uploaded successfully. URL: {}", imageUrl);
+			return root.path("file").path("url").asText();
+		} else {
+			log.error("Failed to upload image to NHN Image Manager. Status: {}, Response: {}", response.getStatusCode(), response.getBody());
+			throw new IOException("Image Manager 업로드 실패");
+		}
+	}
+}
--- src/test/java/shop/nuribooks/books/BooksApplicationTests.java
+++ src/test/java/shop/nuribooks/books/BooksApplicationTests.java
@@ -3,9 +3,13 @@ package shop.nuribooks.books;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.test.context.TestPropertySource;
 
 @EnableDiscoveryClient
 @SpringBootTest
+@TestPropertySource(properties = {
+	"NHN_KEY_MANAGER_SECRET_ACCESS_KEY=your_secret_key"
+})
 class BooksApplicationTests {
 
 	@Test
