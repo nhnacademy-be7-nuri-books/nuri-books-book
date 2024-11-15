ef1f3f75055e941f40b5430aba255ba161cda664
jaaaayk <janghyun30@gmail.com>
2024-11-15 10:28:25 +0900

    refactor: 업데이트 mapping 수정

--- src/main/java/shop/nuribooks/books/book/coupon/controller/MemberCouponController.java
+++ src/main/java/shop/nuribooks/books/book/coupon/controller/MemberCouponController.java
@@ -6,9 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -80,7 +80,7 @@ public class MemberCouponController {
 		@ApiResponse(responseCode = "404", description = "회원 쿠폰을 찾을 수 없음"),
 		@ApiResponse(responseCode = "500", description = "서버 오류")
 	})
-	@PatchMapping("/{memberCouponId}")
+	@PutMapping("/{memberCouponId}")
 	public ResponseEntity<Void> updateIsUsed(@PathVariable Long memberCouponId) {
 		memberCouponService.updateIsUsed(memberCouponId);
 		return ResponseEntity.ok().build();
