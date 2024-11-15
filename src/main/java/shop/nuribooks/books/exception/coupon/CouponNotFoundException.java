66b8565f838e18a54f63062f9b52ec55be22ab43
jaaaayk <janghyun30@gmail.com>
2024-11-15 10:32:03 +0900

    merge: 충돌 핵결

--- src/main/java/shop/nuribooks/books/exception/coupon/CouponNotFoundException.java
+++ src/main/java/shop/nuribooks/books/exception/coupon/CouponNotFoundException.java
@@ -6,7 +6,6 @@ public class CouponNotFoundException extends ResourceNotFoundException {
 	public CouponNotFoundException(String message) {
 		super(message);
 	}
-}
 
 	public CouponNotFoundException() {
 		super("쿠폰이 존재하지 않습니다.");
