4208352b1af140ac4dcfa7b78a08dca1cb53ee44
yeon-so <nordstromm03@gmail.com>
2024-11-18 21:03:37 +0900

    feat: 주문 등록 관련 로직 추가

--- src/main/java/shop/nuribooks/books/order/orderDetail/repository/OrderDetailRepository.java
+++ src/main/java/shop/nuribooks/books/order/orderDetail/repository/OrderDetailRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import shop.nuribooks.books.order.order.entity.Order;
 import shop.nuribooks.books.order.orderDetail.entity.OrderDetail;
 
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long>, OrderDetailCustomRepository {
 }
--- src/main/java/shop/nuribooks/books/order/orderDetail/service/OrderDetailService.java
+++ src/main/java/shop/nuribooks/books/order/orderDetail/service/OrderDetailService.java
@@ -1,7 +1,11 @@
 package shop.nuribooks.books.order.orderDetail.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import shop.nuribooks.books.order.order.entity.Order;
 import shop.nuribooks.books.order.orderDetail.dto.OrderDetailRequest;
+import shop.nuribooks.books.order.orderDetail.entity.OrderDetail;
 
 /**
  * 주문 상세 서비스 인터페이스
@@ -12,4 +16,6 @@ public interface OrderDetailService {
 	void registerOrderDetail(Order order, OrderDetailRequest orderDetailRequest);
 
 	String getBookTitle(Long bookId);
+
+	boolean checkStock(Order order);
 }
