b57a1589b6facbeb107701f0e173ff5ae22ca8c4
yeon-so <115128881+yeon-so@users.noreply.github.com>
2024-11-19 14:57:16 +0900

    feat: 결제 저장 로직 추가 (#301)

--- src/main/java/shop/nuribooks/books/payment/payment/dto/PaymentSuccessRequest.java
+++ src/main/java/shop/nuribooks/books/payment/payment/dto/PaymentSuccessRequest.java
@@ -1,14 +1,18 @@
 package shop.nuribooks.books.payment.payment.dto;
 
-import java.time.OffsetDateTime;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record PaymentSuccessRequest(
 	String status,
 	String orderId,
 	String paymentKey,
-	OffsetDateTime requestedAt,
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	LocalDateTime requestedAt,
 	String method,
-	OffsetDateTime approvedAt,
-	int totalAmount
-)
-{}
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	LocalDateTime approvedAt,
+	Long totalAmount
+) {
+}
