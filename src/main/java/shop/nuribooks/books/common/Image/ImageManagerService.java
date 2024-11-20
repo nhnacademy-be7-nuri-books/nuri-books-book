6ac813d822399f7256016f453eb2c6639690525c
ljg0610 <jggy0627@naver.com>
2024-11-20 20:26:26 +0900

    feat: rabbitmq 설정

--- src/main/java/shop/nuribooks/books/common/Image/ImageManagerService.java
+++ src/main/java/shop/nuribooks/books/common/Image/ImageManagerService.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.	RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.JsonNode;
