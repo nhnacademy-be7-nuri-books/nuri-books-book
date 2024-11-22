85ab42ce7ec5637c31a1af4ba53650451cbd0dde
Yongtaek Lee <143694269+itaekit@users.noreply.github.com>
2024-11-22 09:18:40 +0900

    [Feature/wrapping paper] 포장지 CRUD (관리자) 구현 (#331)
    * chore: WrappingPaper Entity 수정
    
    - @Builder, @Getter 추가
    
    * feat: WrappingPaper DTO 추가
    
    * feat: WrappingPaper CRUD

---
+++ src/main/java/shop/nuribooks/books/order/wrapping/dto/WrappingPaperResponse.java
@@ -0,0 +1,14 @@
+package shop.nuribooks.books.order.wrapping.dto;
+
+import lombok.Builder;
+
+import java.math.BigDecimal;
+
+@Builder
+public record WrappingPaperResponse(
+        Long id,
+        String title,
+        String imageUrl,
+        BigDecimal wrappingPrice
+) {
+}
