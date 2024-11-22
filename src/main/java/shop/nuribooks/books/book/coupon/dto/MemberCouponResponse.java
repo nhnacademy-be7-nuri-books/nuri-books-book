58d899a393af0d99246bf909a30946cf8343d760
jaaaayk <janghyun30@gmail.com>
2024-11-22 17:03:46 +0900

    refactor: MemberCouponResponse 클린

--- src/main/java/shop/nuribooks/books/book/coupon/dto/MemberCouponResponse.java
+++ src/main/java/shop/nuribooks/books/book/coupon/dto/MemberCouponResponse.java
@@ -3,9 +3,7 @@ package shop.nuribooks.books.book.coupon.dto;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import shop.nuribooks.books.book.point.enums.PolicyType;
 
 public record MemberCouponResponse(
