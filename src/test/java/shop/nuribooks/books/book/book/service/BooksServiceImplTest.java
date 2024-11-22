8e1e55bda870e4ffcd46bfcb78b747dacdaa50d2
ljg0610 <jggy0627@naver.com>
2024-11-22 15:26:28 +0900

    fix: 백트래킹 문제 수정 (#350)

--- src/test/java/shop/nuribooks/books/book/book/service/BooksServiceImplTest.java
+++ src/test/java/shop/nuribooks/books/book/book/service/BooksServiceImplTest.java
@@ -617,8 +617,20 @@ public class BooksServiceImplTest {
 	}
 
 	@Test
-	@DisplayName("parseContributors - 작가-역할 파싱 예외처리")
-	public void parseContributorsException() {
+	@DisplayName("parseContributors - 작가-역할 파싱 입력 시 괄호 안닫힘 예외처리")
+	public void parseContributorsNotClosedParenthesisIndex() {
+		String authorRole = "카트가이 (지은이) , 포장지가이 (옮긴이";
+
+		InvalidContributorRoleException exception = assertThrows(InvalidContributorRoleException.class, () -> {
+			ReflectionTestUtils.invokeMethod(bookService, "parseContributors", authorRole);
+		});
+
+		assertEquals("역할 이름이 괄호로 닫히지 않아 작가-역할 저장에 실패했습니다.", exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("parseContributors - 작가-역할 파싱 역할 없음 예외처리")
+	public void parseContributorsExceptionWithoutRole() {
 		String authorRole = "카트가이 (지은이), 포장지가이";
 
 		InvalidContributorRoleException exception = assertThrows(InvalidContributorRoleException.class, () -> {
