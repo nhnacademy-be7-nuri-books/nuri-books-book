e077d8b7e7fc056010ae0b65f39c3879566682d3
jaaaayk <janghyun30@gmail.com>
2024-11-22 09:17:59 +0900

    Feature/member coupon read (#329)
    * feature: 회원페이지 쿠폰 조회용 api 추가
    
    * feat: controller 쉊ㅇ
    
    * feat: swagger 수정
    
    * feat: my-coupon 최종

--- src/main/java/shop/nuribooks/books/book/coupon/repository/querydsl/MemberCouponRepositoryCustom.java
+++ src/main/java/shop/nuribooks/books/book/coupon/repository/querydsl/MemberCouponRepositoryCustom.java
@@ -2,8 +2,15 @@ package shop.nuribooks.books.book.coupon.repository.querydsl;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import shop.nuribooks.books.book.coupon.dto.MemberCouponResponse;
 
 public interface MemberCouponRepositoryCustom {
 	List<MemberCouponResponse> findAllByMemberId(Long memberId);
+
+	Page<MemberCouponResponse> findAvailableCouponsByMemberId(Long memberId, Pageable pageable);
+
+	Page<MemberCouponResponse> findExpiredOrUsedCouponsByMemberId(Long memberId, Pageable pageable);
 }
--- src/main/java/shop/nuribooks/books/book/coupon/repository/querydsl/impl/MemberCouponRepositoryCustomImpl.java
+++ src/main/java/shop/nuribooks/books/book/coupon/repository/querydsl/impl/MemberCouponRepositoryCustomImpl.java
@@ -1,7 +1,13 @@
 package shop.nuribooks.books.book.coupon.repository.querydsl.impl;
 
+import static shop.nuribooks.books.book.coupon.entity.QMemberCoupon.*;
+
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.core.types.Projections;
@@ -23,9 +29,11 @@ public class MemberCouponRepositoryCustomImpl implements MemberCouponRepositoryC
 		QMemberCoupon memberCoupon = QMemberCoupon.memberCoupon;
 
 		return queryFactory.select(Projections.constructor(MemberCouponResponse.class,
-				memberCoupon.id,
-				memberCoupon.coupon.id,
-				memberCoupon.member.id,
+				memberCoupon.coupon.name,
+				memberCoupon.coupon.policyType,
+				memberCoupon.coupon.discount,
+				memberCoupon.coupon.minimumOrderPrice,
+				memberCoupon.coupon.maximumDiscountPrice,
 				memberCoupon.isUsed,
 				memberCoupon.createdAt,
 				memberCoupon.expiredAt
@@ -33,5 +41,77 @@ public class MemberCouponRepositoryCustomImpl implements MemberCouponRepositoryC
 			.from(memberCoupon)
 			.where(memberCoupon.member.id.eq(memberId))
 			.fetch();
+	}
+
+	@Override
+	public Page<MemberCouponResponse> findAvailableCouponsByMemberId(Long memberId, Pageable pageable) {
+		List<MemberCouponResponse> availableCoupons = queryFactory
+			.select(
+				Projections.constructor(
+					MemberCouponResponse.class,
+					memberCoupon.coupon.name,
+					memberCoupon.coupon.policyType,
+					memberCoupon.coupon.discount,
+					memberCoupon.coupon.minimumOrderPrice,
+					memberCoupon.coupon.maximumDiscountPrice,
+					memberCoupon.isUsed,
+					memberCoupon.createdAt,
+					memberCoupon.expiredAt
+				)
+			)
+			.from(memberCoupon)
+			.where(memberCoupon.member.id.eq(memberId)
+				.and(memberCoupon.isUsed.eq(false))
+				.and(memberCoupon.expiredAt.after(LocalDate.now())))
+			.orderBy(memberCoupon.createdAt.desc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		long total = queryFactory
+			.select(memberCoupon.count())
+			.from(memberCoupon)
+			.where(memberCoupon.member.id.eq(memberId)
+				.and(memberCoupon.isUsed.eq(false).or(memberCoupon.expiredAt.after(LocalDate.now())))
+			)
+			.fetchOne();
+
+		return new PageImpl<>(availableCoupons, pageable, total);
+	}
+
+	@Override
+	public Page<MemberCouponResponse> findExpiredOrUsedCouponsByMemberId(Long memberId, Pageable pageable) {
+		List<MemberCouponResponse> expiredCoupons = queryFactory
+			.select(
+				Projections.constructor(
+					MemberCouponResponse.class,
+					memberCoupon.coupon.name,
+					memberCoupon.coupon.policyType,
+					memberCoupon.coupon.discount,
+					memberCoupon.coupon.minimumOrderPrice,
+					memberCoupon.coupon.maximumDiscountPrice,
+					memberCoupon.isUsed,
+					memberCoupon.createdAt,
+					memberCoupon.expiredAt
+				)
+			)
+			.from(memberCoupon)
+			.where(memberCoupon.member.id.eq(memberId)
+				.and(memberCoupon.isUsed.eq(true).or(memberCoupon.expiredAt.loe(LocalDate.now())))
+			)
+			.orderBy(memberCoupon.createdAt.desc())
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		long total = queryFactory
+			.select(memberCoupon.count())
+			.from(memberCoupon)
+			.where(memberCoupon.member.id.eq(memberId)
+				.and(memberCoupon.isUsed.eq(true).or(memberCoupon.expiredAt.loe(LocalDate.now())))
+			)
+			.fetchOne();
+
+		return new PageImpl<>(expiredCoupons, pageable, total);
 	}
 }
