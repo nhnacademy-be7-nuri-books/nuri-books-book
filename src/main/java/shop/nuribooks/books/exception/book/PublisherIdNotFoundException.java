ebcba80b66c30857051ec6eda5ea8af156138958
kyongmin <110990435+kkyongg@users.noreply.github.com>
2024-11-24 11:01:48 +0900

    feat: coupon validation 추가적용 (#361)
    * feat: coupon validation 추가적용
    
    * refactor: 코드 최적화
    
    * refactor: 웰컴쿠폰 체크 조건 제거

--- src/main/java/shop/nuribooks/books/exception/book/PublisherIdNotFoundException.java
+++ src/main/java/shop/nuribooks/books/exception/book/PublisherIdNotFoundException.java
@@ -3,7 +3,7 @@ package shop.nuribooks.books.exception.book;
 import shop.nuribooks.books.exception.ResourceNotFoundException;
 
 public class PublisherIdNotFoundException extends ResourceNotFoundException {
-  public PublisherIdNotFoundException(Long id) {
-    super("해당하는 출판사 ID " + id + " 를 찾을 수 없습니다.");
-  }
+	public PublisherIdNotFoundException(Long id) {
+		super("해당하는 출판사 ID " + id + " 를 찾을 수 없습니다.");
+	}
 }
