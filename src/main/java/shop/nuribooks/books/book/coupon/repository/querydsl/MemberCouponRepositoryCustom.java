6dd82bb2ee0fb2154f0fc83ea93a00d21928f6e9
yeon-so <nordstromm03@gmail.com>
2024-11-25 17:43:00 +0900

    fix: 주문 전용 쿠폰 적용을 위한 로직 추가 및 수정

--- src/main/java/shop/nuribooks/books/book/coupon/repository/querydsl/MemberCouponRepositoryCustom.java
+++ src/main/java/shop/nuribooks/books/book/coupon/repository/querydsl/MemberCouponRepositoryCustom.java
@@ -1,10 +1,12 @@
 package shop.nuribooks.books.book.coupon.repository.querydsl;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import shop.nuribooks.books.book.coupon.dto.MemberCouponOrderDto;
 import shop.nuribooks.books.book.coupon.dto.MemberCouponResponse;
 
 public interface MemberCouponRepositoryCustom {
@@ -13,4 +15,6 @@ public interface MemberCouponRepositoryCustom {
 	Page<MemberCouponResponse> findAvailableCouponsByMemberId(Long memberId, Pageable pageable);
 
 	Page<MemberCouponResponse> findExpiredOrUsedCouponsByMemberId(Long memberId, Pageable pageable);
+
+	List<MemberCouponOrderDto> findAllTypeAvailableCouponsByMemberId(Long memberId, BigDecimal orderTotalPrice);
 }
