6d1ea2899f5fe9fc7648bb49616154b967b81ed7
Yongtaek Lee <143694269+itaekit@users.noreply.github.com>
2024-11-25 17:51:11 +0900

    Feature/shipping 배송 CRUD (#367)
    * feat: 배송 전체 조회(페이징) 및 자세히 조회 구현
    
    * feat: <배송 시작> 기능 구현 및 송장 번호 생성기 구현
    
    * feat: 배송 상태에 따른 주문 상태 변경 동기화 처리

--- src/main/java/shop/nuribooks/books/order/shipping/repository/ShippingRepository.java
+++ src/main/java/shop/nuribooks/books/order/shipping/repository/ShippingRepository.java
@@ -1,5 +1,7 @@
 package shop.nuribooks.books.order.shipping.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import shop.nuribooks.books.order.order.entity.Order;
@@ -8,4 +10,6 @@ import shop.nuribooks.books.order.shipping.entity.Shipping;
 public interface ShippingRepository extends JpaRepository<Shipping, Long> {
 
 	Shipping findByOrder(Order order);
+
+	Page<Shipping> findAll(Pageable pageable);
 }
