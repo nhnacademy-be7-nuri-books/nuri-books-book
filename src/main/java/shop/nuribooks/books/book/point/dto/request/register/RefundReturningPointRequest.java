cc3a89cda627926c2d84ed4aa26bd43c0c063680
Moonsun Park <hxcva1@gmail.com>
2024-11-26 09:54:46 +0900

    Feature/refund 환불 구현 (#373)
    * feat: 간단한 환불 로직 구현
    
    * feat: 환불 정보 조회 기능 추가
    
    * feat: 전체 반품만 가능하도록 변경
    
    * feat: 환불 구현
    
    * test: 환불 테스트 수정

--- src/main/java/shop/nuribooks/books/book/point/dto/request/register/RefundReturningPointRequest.java
+++ src/main/java/shop/nuribooks/books/book/point/dto/request/register/RefundReturningPointRequest.java
@@ -1,30 +1,32 @@
-// package shop.nuribooks.books.book.point.dto.request.register;
-//
-// import jakarta.validation.constraints.NotNull;
-// import shop.nuribooks.books.book.point.entity.PointHistory;
-// import shop.nuribooks.books.book.point.entity.PointPolicy;
-// import shop.nuribooks.books.book.point.entity.child.RefundReturningPoint;
-// import shop.nuribooks.books.member.member.entity.Member;
-//
-// public class RefundReturningPointRequest extends PointHistoryRequest {
-// 	@NotNull
-// 	Refund refund;
-// 	PointHistory savedPointHistory;
-//
-// 	public RefundReturningPointRequest(Member member, Refund refund, PointHistory savedPointHistory) {
-// 		super(member);
-// 		this.refund = refund;
-// 		// 이전 주문에서 적립된 금액만큼 빼줘야함. 환불에서 취소된 금액도 찾아와야함.
-// 		this.savedPointHistory = savedPointHistory;
-// 	}
-//
-// 	public RefundReturningPoint toEntity(PointPolicy pointPolicy) {
-// 		return RefundReturningPoint.builder()
-// 			.amount(refund.amount().minus(savedPointHistory.getAmount()))
-// 			.description("환불 반환 포인트. (사용한 포인트 포함, 적립된 포인트 제외)")
-// 			.member(member)
-// 			.pointPolicy(pointPolicy)
-// 			.refund(refund)
-// 			.build();
-// 	}
-// }
+package shop.nuribooks.books.book.point.dto.request.register;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotNull;
+import shop.nuribooks.books.book.point.entity.PointPolicy;
+import shop.nuribooks.books.book.point.entity.child.RefundReturningPoint;
+import shop.nuribooks.books.member.member.entity.Member;
+import shop.nuribooks.books.order.refund.entity.Refund;
+
+public class RefundReturningPointRequest extends PointHistoryRequest {
+	@NotNull
+	Refund refund;
+	BigDecimal amount;
+
+	public RefundReturningPointRequest(Member member, Refund refund, BigDecimal amount) {
+		super(member);
+		this.refund = refund;
+		// 이전 주문에서 적립된 금액만큼 빼줘야함. 환불에서 취소된 금액도 찾아와야함.
+		this.amount = amount;
+	}
+
+	public RefundReturningPoint toEntity(PointPolicy pointPolicy) {
+		return RefundReturningPoint.builder()
+			.amount(amount)
+			.description("환불 반환 포인트. (사용한 포인트 포함, 적립된 포인트 제외)")
+			.member(member)
+			.pointPolicy(pointPolicy)
+			.refund(refund)
+			.build();
+	}
+}
--- src/main/java/shop/nuribooks/books/book/point/entity/child/RefundReturningPoint.java
+++ src/main/java/shop/nuribooks/books/book/point/entity/child/RefundReturningPoint.java
@@ -1,25 +1,26 @@
-// package shop.nuribooks.books.book.point.entity.child;
-//
-// import jakarta.persistence.DiscriminatorValue;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.JoinColumn;
-// import jakarta.persistence.OneToOne;
-// import jakarta.persistence.Table;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.experimental.SuperBuilder;
-// import shop.nuribooks.books.book.point.entity.PointHistory;
-//
-// @Entity
-// @Getter
-// @NoArgsConstructor
-// @SuperBuilder
-// @DiscriminatorValue("refund_returning_point")
-// @Table(name = "refund_returning_point")
-// public class RefundReturningPoint extends PointHistory {
-// 	// TODO:: 환불 id 추가
-// 	@OneToOne
-// 	@JoinColumn(name = "refund_id")
-// 	private Refund refund;
-//
-// }
+package shop.nuribooks.books.book.point.entity.child;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import shop.nuribooks.books.book.point.entity.PointHistory;
+import shop.nuribooks.books.order.refund.entity.Refund;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@DiscriminatorValue("refund_returning_point")
+@Table(name = "refund_returning_point")
+public class RefundReturningPoint extends PointHistory {
+	// TODO:: 환불 id 추가
+	@OneToOne
+	@JoinColumn(name = "refund_id")
+	private Refund refund;
+
+}
