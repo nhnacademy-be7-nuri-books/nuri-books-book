cb7ab3a56cdc33a1b4971ba7ed5349959a3db5c9
jaaaayk <janghyun30@gmail.com>
2024-12-05 09:33:23 +0900

    feat: CouponMapper 쿼리에 적용

--- src/main/java/shop/nuribooks/books/book/coupon/repository/querydsl/impl/CouponCustomRepositoryImpl.java
+++ src/main/java/shop/nuribooks/books/book/coupon/repository/querydsl/impl/CouponCustomRepositoryImpl.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
@@ -14,11 +13,13 @@ import shop.nuribooks.books.book.coupon.dto.CouponResponse;
 import shop.nuribooks.books.book.coupon.entity.Coupon;
 import shop.nuribooks.books.book.coupon.entity.QCoupon;
 import shop.nuribooks.books.book.coupon.enums.CouponType;
+import shop.nuribooks.books.book.coupon.mapper.CouponMapper;
 import shop.nuribooks.books.book.coupon.repository.querydsl.CouponCustomRepository;
 
 @RequiredArgsConstructor
 public class CouponCustomRepositoryImpl implements CouponCustomRepository {
 	private final JPAQueryFactory queryFactory;
+	private final CouponMapper couponMapper;
 
 	@Override
 	public Coupon findCouponsByNameLike(String name) {
@@ -35,26 +36,18 @@ public class CouponCustomRepositoryImpl implements CouponCustomRepository {
 	public Page<CouponResponse> findCouponsByCouponType(Pageable pageable, CouponType type) {
 		QCoupon coupon = QCoupon.coupon;
 
-		List<CouponResponse> results = queryFactory.select(Projections.constructor(CouponResponse.class,
-				coupon.id,
-				coupon.name,
-				coupon.couponPolicy.discountType,
-				coupon.couponPolicy.discount,
-				coupon.couponPolicy.minimumOrderPrice,
-				coupon.couponPolicy.maximumDiscountPrice,
-				coupon.createdAt,
-				coupon.expiredAt,
-				coupon.period,
-				coupon.expirationType,
-				coupon.deletedAt,
-				coupon.couponType
-			))
-			.from(coupon)
+		List<Coupon> coupons = queryFactory.selectFrom(coupon)
 			.where(coupon.couponType.eq(type))
 			.offset(pageable.getOffset())
 			.limit(pageable.getPageSize())
 			.fetch();
 
+		// 엔티티 -> DTO 변환
+		List<CouponResponse> results = coupons.stream()
+			.map(couponMapper::toDto) // CouponMapper 활용
+			.toList();
+
+		// 총 개수 조회
 		long total = queryFactory.select(coupon.count())
 			.from(coupon)
 			.where(coupon.couponType.eq(type))
