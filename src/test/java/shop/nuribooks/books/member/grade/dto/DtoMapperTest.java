f462df7ed8936b46b76d43d99dddc7b5329e6bec
Jprotection <bigho98@gmail.com>
2024-12-05 16:15:23 +0900

    test: member 관련 테스트 코드 추가 (#440)
    * test: 테스트 코드 추가
    
    * refactor: 사용하지 않는 예외 삭제

---
+++ src/test/java/shop/nuribooks/books/member/grade/dto/DtoMapperTest.java
@@ -0,0 +1,23 @@
+package shop.nuribooks.books.member.grade.dto;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DtoMapperTest {
+
+	@DisplayName("DtoMapper 클래스는 인스턴스를 생성할 수 없으며, 기본 생성자는 예외를 던진다.")
+	@Test
+	void testDtoMapperConstructor() throws NoSuchMethodException {
+		// 리플렉션을 이용하여 private 생성자에 접근
+		Constructor<DtoMapper> constructor = DtoMapper.class.getDeclaredConstructor();
+		constructor.setAccessible(true); // private 접근을 허용
+
+		Assertions.assertThatThrownBy(constructor::newInstance)
+			.isInstanceOf(InvocationTargetException.class) // InvocationTargetException이 발생해야 한다.
+			.hasCauseInstanceOf(UnsupportedOperationException.class); // 내부 예외가 UnsupportedOperationException이어야 한다.
+	}
+}
